f5c862d4771779e6514fee6caaf3c893f08fa07b
Matias D Brites <104856701+MatiasDBrites@users.noreply.github.com>
2022-08-08 13:40:38 -0500

    Update Desafio.js

--- Desafio.js
+++ Desafio.js
@@ -7,9 +7,9 @@ function crearGato (nombre, edad) {
   // Devuelve el objeto
   // Tu código:
   let gato = {
-    "nombre": "nombre",
-    "edad": "edad",
-    "meow": function meow() {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
       return "Meow!";
     }
   }
@@ -21,7 +21,7 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto [property] = "null";
+  objeto[property] = null;
   return objeto; 
 }
 
@@ -30,7 +30,7 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-  objeto[metodo]();  
+  objeto[metodo()];  
 }
 
 
